88154ed83cecee45553f3a23cbb89ed03995fb33
ScuberKiwi <66200947+StephenPowick@users.noreply.github.com>
2025-07-23 21:13:38 +1200

    Commit 7
    9:13 23/7/25

--- Assets/Scenes/playerdialougemanager.gd
+++ Assets/Scenes/playerdialougemanager.gd
@@ -1,9 +1,25 @@
 extends Area2D
 
+var inbody
+var npc
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.name == "NPC1":
+		inbody = true
+		npc = area.name
+		
 
 
-#
-#func _input(event):
-	#if player_is_in_area and Input.is_action_pressed("interact"):
-			#if Dialogic.current_timeline == null:
-				#Dialogic.start("island1pirate1")
+
+
+func _on_area_exited(area: Area2D) -> void:
+		npc = null
+		inbody = false
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact"):
+		if Dialogic.current_timeline != null:
+			return
+		if npc == "NPC1":
+			Dialogic.start('island1pirate1')
