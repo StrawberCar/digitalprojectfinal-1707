059a759c442fdbf7510088dafcd03f0b463735b5
StrawberCar <147669151+StrawberCar@users.noreply.github.com>
2025-07-23 17:29:22 +1200

    Commit 5
    Bleh

---
+++ Assets/Scenes/playerdialougemanager.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+
+
+#
+#func _input(event):
+	#if player_is_in_area and Input.is_action_pressed("interact"):
+			#if Dialogic.current_timeline == null:
+				#Dialogic.start("island1pirate1")
